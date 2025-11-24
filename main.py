f88db696056933f54050c09bf6c7cc8880991d92
Akshay-Pradh <82466825+Akshay-Pradh@users.noreply.github.com>
2025-11-24 10:42:25 -0500

    thruster xml updates

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import mujoco
 import mujoco.viewer
 import time
 import numpy as np
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+xml_path = os.path.join(BASE_DIR, "project.xml")
 
-model = mujoco.MjModel.from_xml_path('project.xml')
+model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 mujoco.mj_resetData(model, data)
-data.qvel[3:6] = 5*np.random.randn(3)
+# data.qvel[3:6] = 5*np.random.randn(3)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     dt = model.opt.timestep
